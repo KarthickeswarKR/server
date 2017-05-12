6781e683ec965d479d6b94fa2dd27ea117987aca
karthickeswar <karthickeswar@virtualgodown.com>
2017-05-12 12:48:19 +0545

    app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var users = require('./app/controller/user-controller');
 var vg = express();
+var config = require('./app/config/config');
+var vg = express();
+var port = process.env.PORT|| config.get('port');
 vg.use('/api', users);
 vg.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-var port=80;
 module.exports = vg;
 vg.listen(port);
 console.log("vg is started and listening");
