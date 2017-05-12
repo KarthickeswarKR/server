e09897f82998fe2751462f46dbf829d99f914f0f
manikandan0625 <s.manikandan@virtualgodown.com>
2017-05-12 12:19:48 +0545

    Revert "commitsadfdsgft"
    This reverts commit 23fdc8420ce0114f4d38d347854f18670fa93f2f.

--- app/config/database.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-  url: 'mongodb://104.197.225.207:27017/vg'
-};
--- app/config/logconfig.js
+++
@@ -1,9 +0,0 @@
-var nconf = require('nconf');
-
-nconf.argv()
-	.env()
-	.file({
-		file: process.cwd() + '/log4js.json'
-	});
-
-module.exports = nconf;
